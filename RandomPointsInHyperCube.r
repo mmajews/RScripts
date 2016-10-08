532b7d33e3c9516a4bfece6d868b404dbb67fd21
Marcin Majewski <majewskimarcin@outlook.com>
2016-10-08 22:39:19 +0200

    * Reseting hyper cube to correct version

--- RandomPointsInHyperCube.r
+++ RandomPointsInHyperCube.r
@@ -1,58 +1,75 @@
 library(stargazer)
-library("plotrix")
 
 side_lenght_hypercube <- 10
-number_of_points_to_generate <-1500
+number_of_points_to_generate <-1000
 maximum_dimension <- 200
-minimum_dimension <- 3
-
-all_points_in_cube = list()
-all_points_in_sphere = list()
+minimum_dimension <- 2
+corner_of_hypercube = NA
 
 
+parameters_beetwen_points = matrix(NA, nrow = 200, ncol = 3)
+parameters_beetwen_point_and_corner = matrix(NA, nrow = 200, ncol = 3)
+
+colnames(parameters_beetwen_points) <- c('Dimension', 'Variance', 'Expected Value')
+colnames(parameters_beetwen_point_and_corner) <- c('Dimension', 'Variance', 'Expected Value')
+
+all_distances_beetwen_points = list()
+all_distances_beetwen_point_and_corner = list()
 
 for(current_dimension in minimum_dimension:maximum_dimension)
 {
-  selected_points_cube <- matrix(NA,number_of_points_to_generate, current_dimension)
-  selected_points_sphere <- matrix(NA,number_of_points_to_generate, current_dimension)
+  corner_of_hypercube = rep(side_lenght_hypercube, current_dimension)
+  randomly_selected_points <- matrix(NA,2 * number_of_points_to_generate, current_dimension)
+  distances_beetwen_points <- matrix(NA, number_of_points_to_generate, 1)
+  distances_beetwen_point_and_corner <- matrix(NA, number_of_points_to_generate, 1)
   
-  #Hypercube generation
+  row_number <- 0
   for (i in 1:number_of_points_to_generate){
-    coordinates <- runif(current_dimension, -side_lenght_hypercube/2, side_lenght_hypercube/2)
-    selected_points_cube[i,] <- coordinates
+    coordinates_1 <- runif(current_dimension, 0.0, side_lenght_hypercube)
+    randomly_selected_points[row_number,] <- coordinates_1
+    row_number <- row_number + 1
+    
+    distances_beetwen_point_and_corner[i,] <- dist(rbind(coordinates_1, corner_of_hypercube))
+    
+    coordinates_2 <- runif(current_dimension, 0.0, side_lenght_hypercube)
+    randomly_selected_points[row_number,] <- coordinates_2
+    row_number <- row_number + 1
+    
+    distances_beetwen_points[i,] <- dist(rbind(coordinates_1, coordinates_2))
   }
   
-  #Hypershpere generation
-  for (i in 1:number_of_points_to_generate){
-    coordinates <- runif(current_dimension, -side_lenght_hypercube/2, side_lenght_hypercube/2)
-    coefficient <- 0
-    for(elem in coordinates) {
-      coefficient <- coefficient + elem^2
-    }
-    coefficient <- sqrt(coefficient)
-    coordinates <- (side_lenght_hypercube/2 * (runif(n = 1, min = 0, max = 1))^(1/current_dimension)/coefficient) * coordinates
-    selected_points_sphere[i,] <- coordinates
-  }
+  variance <- var(distances_beetwen_points)
+  expected_value <- weighted.mean(distances_beetwen_points, rep(0.001, 1000))
+  parameters_beetwen_points[current_dimension, ] <- c(current_dimension, variance, expected_value)
+  all_distances_beetwen_points[[current_dimension]] <- distances_beetwen_points
   
-  all_points_in_sphere[[current_dimension]] <-selected_points_sphere
-  all_points_in_cube[[current_dimension]] <-selected_points_cube
+  variance_point_corner <- var(distances_beetwen_point_and_corner)
+  expected_value_point_corner <- weighted.mean(distances_beetwen_point_and_corner, rep(0.001, 1000))
+  parameters_beetwen_point_and_corner[current_dimension, ] <- c(current_dimension, variance_point_corner, expected_value_point_corner)
+  all_distances_beetwen_point_and_corner[[current_dimension]] <-distances_beetwen_point_and_corner
+  
+  remove(randomly_selected_points)
+  remove(distances_beetwen_points)
+  remove(distances_beetwen_point_and_corner)
 }
 
+dimensions =c(2,10,50,100,150,200)
+#Drawing for hypercube distances between points
 
-#Generate plots with given dimensions
-dimensions = c(10, 50 , 100, 150 ,200)
 for(dimension in dimensions){
-  points_in_cube <- all_points_in_cube[[dimension]]
-  points_in_cube <- points_in_cube[,c(1,2)]
-  
-  points_in_sphere = all_points_in_sphere[[dimension]]
-  points_in_sphere <- points_in_sphere[,c(1,2)]
-  
-  max_coord <- side_lenght_hypercube/2
-  
-  plot(-max_coord:max_coord, -max_coord:max_coord, type = "n", asp = 1, main = paste("n=",toString(dimension),sep=""))
-  points(points_in_cube[,1], points_in_cube[,2], col = "red")
-  points(points_in_sphere[,1], points_in_sphere[,2], col = "blue")
-  draw.circle(0, 0, max_coord, border = "blue")
-  rect(-max_coord, -max_coord, max_coord, max_coord, border = "red")
+  png(file = paste(toString(dimension),"_point_to_point.png",sep="") )
+  hist(as.vector(all_distances_beetwen_points[dimension][[1]]), main = paste("n=",toString(dimension),sep=""),xlab = "Distance between points", col="darkgreen")
+  dev.off()
 }
+
+#Drawing for hypercube distances between point and fixed corner
+for(dimension in dimensions){
+  png(file = paste(toString(dimension),"_point_to_corner.png",sep="")) 
+  hist(as.vector(all_distances_beetwen_point_and_corner[dimension][[1]]), main = paste("n=",toString(dimension),sep=""),xlab = "Distance between point and corner", col="darkgreen")
+  dev.off()
+}
+
+#Table outputs
+stargazer(parameters_beetwen_points, type = "text", title="Point to point statistics", digits=1, out="point_to_point.txt")
+stargazer(parameters_beetwen_point_and_corner, type = "text", title="Point to corner statistics", digits=1, out="point_to_corner.txt")
+
